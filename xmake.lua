1c01ceefcb9933fdd477d01ae5407fafbcf87fb7
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-12-29 17:24:39 +0100

    improve xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,6 @@
 target("extlib2")
     set_kind("static")
     set_languages("cxxlatest", "clatest")
-    add_headerfiles("include/(extlib/*.hpp)")
-    add_includedirs("include", {public = true})
     add_files("modules/**.mpp", {install = true})
     add_files("src/**.cpp")
+²
