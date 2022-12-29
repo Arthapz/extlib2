b02724edddad8d389353dc3dc93c6414c28a9b4e
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-12-29 18:29:31 +0100

    add compatibility with gcc and clang

--- xmake.lua
+++ xmake.lua
@@ -3,3 +3,7 @@ target("extlib2")
     set_languages("cxxlatest", "clatest")
     add_files("modules/**.mpp", {install = true})
     add_files("src/**.cpp")
+
+    if is_plat("windows") then
+        add_defines("USE_STD_MODULE")
+    end
